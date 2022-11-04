3b3f5870375dbe2a8accb45ba8fe32d9abb3c337
Taylher <alexmota@alu.ufc.br>
2022-11-04 15:48:49 -0300

    teste-commit

--- express-cookies/index.js
+++ express-cookies/index.js
@@ -1,96 +1,140 @@
-const express = require('express')
-var cookieSession = require('cookie-session')
-const app = express()
-const port = 3000
+const express = require("express");
+var cookieSession = require("cookie-session");
+const app = express();
+const port = 3000;
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.use(cookieSession({
-  name: 'ufc-web-session',
-  secret: 'c293x8b6234z82n938246bc2938x4zb234',
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+app.use(
+  cookieSession({
+    name: "ufc-web-session",
+    secret: "c293x8b6234z82n938246bc2938x4zb234",
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 
-app.get('/logout', (req, res) => {
-  req.session = null
-  res.redirect('/')
-})
+app.get("/logout", (req, res) => {
+  req.session = null;
+  res.redirect("/");
+});
 
 app.use((req, res, next) => {
-  console.log('== Session Log Middleware');
-  console.log(req.session);
+  //console.log("== Session Log Middleware");
+  //console.log(req.session);
   next();
 });
 
-app.use((req, res, next) => {
-  console.log('== Root Middleware');
-  if(req.path == '/'){
-    if(req.session.user){
-      console.log('Go home')
-      res.redirect('/home')
-    } else next();
-  } else next();
+// app.use((req, res, next) => {
+//   // console.log("== Root Middleware");
+//   if (req.path == "/") {
+//     if (req.session.user) {
+//       // console.log("Go home");
+//       res.redirect("/home");
+//     } else next();
+//   } else next();
+// });
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const mongoRepository = require("./repository/mongo-repository");
+
+app.get("/", (req, res) => {
+  res.render("raiz/home");
 });
 
-app.use(express.static('public'));
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-const mongoRepository = require('./repository/mongo-repository')
-
-app.post('/login', (req, res) => {
-  console.log('post - /login')
-  console.log(req.body)
-  mongoRepository.getUsers(req.body.login, req.body.pass).then(users => {
-    if (users.length > 0){
-      req.session.user = users[0]
-      res.redirect('/home')
-      return 
-    } else res.redirect('/')
+app.post("/form-login", (req, res) => {
+  // console.log("post - /login");
+  // console.log(req.body);
+  mongoRepository.getUsers(req.body.login, req.body.pass).then((users) => {
+    if (users.length > 0) {
+      req.session.user = users[0];
+      res.redirect("/loja");
+      return;
+    } else res.redirect("/");
   });
-})
+});
 
 app.use((req, res, next) => {
-  console.log('== Auth Middleware')
-  console.log(req.session)
-  if(req.session.user){
-    next()
-  }
-  else res.redirect('/')
+  console.log("User Authenticator");
+  console.log(req.session);
+  if (req.session.user) {
+    next();
+  } else res.redirect("/");
+});
 
-})
+app.use("/loja/*", (req, res, next) => {
+  // console.log("== Student Area Middleware");
+  console.log("req.session.user.role - ", req.session.user.role);
+  if (req.session.user.role == "student") {
+    next();
+  } else res.redirect("/home");
+});
 
-app.get('/home',(req,res) => {
-  res.render('home', {user: req.session.user})
-})
+app.get("/loja", async (req, res) => {
+  const ListCars = await mongoRepository.getAllCars();
+  console.log("req.session.user.role - >", req.session.user.role);
+  console.log("Nome do usuario - >", req.session.user.username);
 
-app.use('/student/*',(req, res, next) => {
-  console.log('== Student Area Middleware')
-  console.log(req.session)
-  if(req.session.user.role == 'student'){
-    next()
-  }
-  else res.redirect('/home')
-})
+  res.render("loja/home", { user: req.session.user, cars: ListCars });
+});
 
-app.get('/student/home',(req,res) => {
-  res.render('student/home', {user: req.session.user})
-})
+app.use("/admin/*", (req, res, next) => {
+  // console.log("== Admin Area Middleware");
+  // console.log(req.session);
+  console.log("session.user.role - ", req.session.user.role);
+  if (req.session.user.role == "admin") {
+    next();
+  } else res.redirect("/home");
+});
 
-app.use('/admin/*',(req, res, next) => {
-  console.log('== Admin Area Middleware')
-  console.log(req.session)
-  if(req.session.user.role == 'admin'){
-    next()
-  }
-  else res.redirect('/home')
-})
+app.get("/admin/home", (req, res) => {
+  res.render("admin/home", { user: req.session.user });
+});
 
-app.get('/admin/home',(req,res) => {
-  res.render('admin/home', {user: req.session.user})
-})
+app.get("/cars", async (req, res) => {
+  const ListCars = await mongoRepository.getAllCars();
+  console.log("carros", ListCars[0]);
+  res.render("car/home", { cars: ListCars });
+});
+
+app.get("/signup", (req, res) => {
+  //console
+  res.render("login/home");
+});
+
+app.get("/signin", (req, res) => {
+  //console
+  res.render("cadastro/home");
+});
+
+app.post("/form-cadastro-users", async (req, res) => {
+  const { username, email, password, confirmPassword, genero, data } = req.body;
+  console.log(username, email, password, confirmPassword, genero, data);
+  // type UserData = {
+  //   nome: String;
+  //   email: String;
+  //   senha: String;
+  //   confirmacaoSenha: String;
+  //   genero: String;
+  //   data: String;
+  // }
+  const user = {
+    username,
+    email,
+    password,
+    confirmPassword,
+    role: "student",
+    genero,
+    data,
+  };
+  const result = await mongoRepository.setUsers(user);
+  console.log("Deu certo registrar = ", result);
+  res.redirect("/home");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- express-cookies/repository/mongo-repository.js
+++ express-cookies/repository/mongo-repository.js
@@ -1,33 +1,46 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 
 // Connection URL
-const url = 'mongodb://root:rootpwd@localhost:27017';
+const url = "mongodb://root:rootpwd@localhost:27017";
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'ufcweb2022';
+const dbName = "ufcweb2022";
 
 var user_collection;
+var car_collection;
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
-  console.log('Connected successfully to Mongo server');
+  console.log("Connected successfully to Mongo server");
   const db = client.db(dbName);
-  user_collection = db.collection('user');
+  user_collection = db.collection("user");
+  car_collection = db.collection("car");
+
   // the following code examples can be pasted here...
-   
-  return 'done.';
+
+  return "done.";
 }
 
-main()
-  .then(console.log)
-  .catch(console.error);
+main().then(console.log).catch(console.error);
 //   .finally(() => client.close());
 
 async function getUsers(username, password) {
-    const findResult = await user_collection.find({username: username, password: password}).toArray();
-    console.log('Found documents =>', findResult);
-    return findResult;
+  const findResult = await user_collection
+    .find({ username: username, password: password })
+    .toArray();
+  console.log("Found documents =>", findResult);
+  return findResult;
+}
+async function setUsers(user) {
+  const result = await user_collection.insertOne(user);
+  return result;
+}
+async function getAllCars() {
+  const findResult = await car_collection.find().toArray();
+  return findResult;
 }
 
 exports.getUsers = getUsers;
+exports.getAllCars = getAllCars;
+exports.setUsers = setUsers;
